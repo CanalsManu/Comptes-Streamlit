9b2979de4b4afff332875f375ab896af5b8ecdc5
CanalsManu <canalsmanu@gmail.com>
2026-04-22 18:48:15 +0200

    Show data and classification tree online

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-uploaded_file = st.file_uploader('Upload your file please', type=('csv', 'py'))
+uploaded_file = st.file_uploader('Puja el teu fitxer de comptes si us plau', type=('csv'))
 
 if uploaded_file is not None:
     st.write('File uploaded.')
+    df = pd.read_csv(uploaded_file)
+    columns = df.columns.tolist()
+
+    st.header('Show data')
+    st.write(df[[col for col in columns if col!= 'Classificació']])
+
+    st.header('Show classification tree')
+    st.write(df['Classificació'])
 else:
     st.write('Waiting on file upload...')
