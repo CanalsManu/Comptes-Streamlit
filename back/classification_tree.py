313c0ed1fbab1408094fb607995debe112104bdf
Manu Canals <manu.canals@lacelithography.com>
2026-05-07 19:46:25 +0200

    classification tree from list/series

---
+++ back/classification_tree.py
@@ -0,0 +1,44 @@
+def dashed_to_tree(dashed):
+    """
+    Get classification tree from list or Series of dashed classification.
+    
+    Tree is a dictionary, where None indicates the end of one branch.
+    """
+    if not isinstance(dashed, list):  # assume is then pd.Series
+        dashed = dashed.tolist()
+
+    tree = {}
+    for item in dashed:
+        split = item.split('-')
+        multi_key = ''.join(f'["{k}"]' for k in split)
+        assert 'tree' in locals()
+
+        # If try successeeds -> branch already exists -> continue
+        try:
+            eval('tree' + multi_key)
+            continue
+
+        # If it fails -> split is a new branch -> add it
+        except KeyError:
+            tree = _add_branch_to_tree(tree, split)
+
+    return tree
+
+        
+def _add_branch_to_tree(tree, branch):
+    """
+    Sets a value in a nested dictionary, creating intermediate 
+    dictionaries if they do not exist.
+    """
+    current = tree
+    # Iterate through the path except for the very last key
+    for key in branch[:-1]:
+        # If the key isn't there, or isn't a dictionary, create one
+        if key not in current or not isinstance(current[key], dict):
+            current[key] = {}
+        current = current[key]
+    
+    # Set the value for the final key in the path
+    current[branch[-1]] = None
+    return tree
+
--- front/homepage.py
+++ front/homepage.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import pandas as pd
+from back.classification_tree import dashed_to_tree
 
 @st.dialog("Hola!", dismissible=False)
 def init_dialog():
     uploaded_file = st.file_uploader('Puja el teu fitxer de comptes, si us plau', type=('csv'))
 
     if uploaded_file is not None:
-        st.session_state['db'] = pd.read_csv(uploaded_file)
+        db = pd.read_csv(uploaded_file)
+        st.session_state['db'] = db
+        clsf = db['Classificació']
+        print('b')
+        st.session_state['clsf_tree'] = dashed_to_tree(clsf[~clsf.isnull()])
         st.rerun()
 
     if st.button("Close"):
