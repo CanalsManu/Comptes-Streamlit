1adc5ed37417d85a0d912515581dfa6fb837e7cb
Manu Canals <manu.canals@lacelithography.com>
2026-05-18 20:05:45 +0200

    Fix bug/feature: go to homepage after refresh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import os
 
 homepage = st.Page(os.path.join(os.getcwd(), 'front', 'homepage.py'),
@@ -9,5 +8,11 @@ cla_tree = st.Page(os.path.join(os.getcwd(), 'front', 'classification_tree.py'))
 analysis = st.Page(os.path.join(os.getcwd(), 'front', 'analysis.py'))
 add_moves = st.Page(os.path.join(os.getcwd(), 'front', 'add_movements.py'))
 
-pg = st.navigation([homepage, database, cla_tree, add_moves, analysis])
-pg.run() 
+pg = st.navigation([homepage, database, cla_tree, add_moves, analysis],
+                   position='sidebar')
+
+if 'assigned_at_refresh' in st.session_state:
+    pg.run()
+else:  # Refreshed, assign flag and run homepage
+    st.session_state['assigned_at_refresh'] = True
+    st.switch_page(homepage)
